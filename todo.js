554323d7bcf3e4873eb2374439dd75ccc3ca84de
Derek Manning <61208639+ManningD24@users.noreply.github.com>
2023-04-19 12:39:24 -0500

    Improved styling

--- todo.js
+++ todo.js
@@ -1,28 +1,34 @@
 let id = 0;
+const newStartDateInput = document.getElementById('new-start-date');
+const newEndDateInput = document.getElementById('new-end-date');
+const newTaskInput = document.getElementById('new-task');
+const listTable = document.getElementById('list');
 
-document.getElementById('add').addEventListener('click', () => {
-    let createdDate = new Date();
-    let table = document.getElementById('list');
-    let row = table.insertRow(1);
-    row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-task').value;
-    row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
-    row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
-    row.insertCell(3).innerHTML = document.getElementById('new-end-date').value;
-    let action = row.insertCell(4);
-    action.appendChild(createDeleteButton(id++));
-    document.getElementById('new-task').value = ''
-});
+document.getElementById('add').addEventListener('click', createNewToDoItem);
+
+function createNewToDoItem() {
+  const createdDate = new Date();
+  const newRow = listTable.insertRow(1);
+  newRow.setAttribute('id', `item-${id}`);
+  newRow.insertCell(0).innerHTML = newTaskInput.value;
+  newRow.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
+  newRow.insertCell(2).innerHTML = newStartDateInput.value;
+  newRow.insertCell(3).innerHTML = newEndDateInput.value;
+  const actionsCell = newRow.insertCell(4);
+  actionsCell.appendChild(createDeleteButton(id));
+  newTaskInput.value = '';
+  id++;
+}
 
 function createDeleteButton(id) {
-    let btn = document.createElement('button');
-    btn.className = 'btn btn-primary';
-    btn.id = id;
-    btn.innerHTML = 'Delete';
-    btn.onclick = () => {
-        console.log(`Deleting row with id: item-${id}`)
-        let elementToDelete = document.getElementById(`item-${id}`)
-        elementToDelete.parentNode.removeChild(elementToDelete);
-    };
-    return btn;
+  const btn = document.createElement('button');
+  btn.className = 'btn btn-primary';
+  btn.id = id;
+  btn.innerHTML = 'Delete';
+  btn.onclick = () => {
+    console.log(`Deleting row with id: item-${id}`);
+    const elementToDelete = document.getElementById(`item-${id}`);
+    elementToDelete.parentNode.removeChild(elementToDelete);
+  };
+  return btn;
 }
